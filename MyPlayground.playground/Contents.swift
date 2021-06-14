9517c871164436097c1515d724d14177df128c2e
Joao Silva <josejoaosilva.n@gmail.com>
2021-06-14 15:48:16 -0300

    day01: variables, simple data types, and string interpolation

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "Hello, playground" // String type
+str = "Goodbye" // change variable value
+
+var age = 38  // Int type
+var population = 80_000_000  // Int type
+
+var mStr = """
+    lot
+    of
+    lines
+"""
+
+var mStr2 = """
+    one \
+    line
+"""
+
+var pi = 3.141  // Double type
+var awesome = true // Boolean type
+
+var score = 10
+var scoreString = "You score was \(score)" // String interpolatiopn
+
+
+let const = "Constant value" // String value into let
+
+let number: Int = 10 // type annotation
