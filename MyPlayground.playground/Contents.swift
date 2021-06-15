5c328a3a01cf4375fd6803f338d1c0e500f96cc9
Joao Silva <josejoaosilva.n@gmail.com>
2021-06-15 20:30:04 -0300

    arrays, dictionaries, sets, and enums

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,29 +1,51 @@
 import UIKit
 
-var str = "Hello, playground" // String type
-str = "Goodbye" // change variable value
+let joao = "Joao"
+let github = "jjoaosilva"
 
-var age = 38  // Int type
-var population = 80_000_000  // Int type
-
-var mStr = """
-    lot
-    of
-    lines
-"""
-
-var mStr2 = """
-    one \
-    line
-"""
-
-var pi = 3.141  // Double type
-var awesome = true // Boolean type
-
-var score = 10
-var scoreString = "You score was \(score)" // String interpolatiopn
+let array: [String] = [joao, github, "this is my data"]
 
 
-let const = "Constant value" // String value into let
+let mySet = Set([joao, github])
 
-let number: Int = 10 // type annotation
+var meLikeTuple = (name: joao, github: github)
+
+let myDictionary: [String: String] = [
+    "name": joao,
+    "github": github
+]
+
+print(myDictionary["name"])
+print(myDictionary["favoriteFood", default: "Chocolate"])
+
+let emptyArray1 = [String]()
+let emptyArray2 = Array<Int>()
+
+let emptyDictionary1 = [String:String]()
+let emptyDictionary2 = Dictionary<Int, Int>()
+
+let empitySet = Set<String>()
+
+enum Result {
+    case success
+    case failure
+}
+
+let result = Result.success
+
+enum Activity { // associated values
+    case bored
+    case running(destination: String)
+}
+
+let firstActivity = Activity.running(destination: "Home")
+let secondActivity = Activity.running(destination: "Gym")
+
+enum Planets: Int { // raw values
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planets.init(rawValue: 3)
